b894ae8cef018e7781560a30d0eb07a10dbc4ba0
mustehsan <mustehsanali512@gmail.com>
2025-09-19 16:38:14 +0500

    [ADD] the video links

--- src/app/components/HeroSection/HeroVideo.js
+++ src/app/components/HeroSection/HeroVideo.js
@@ -2,7 +2,7 @@ import LeftServicesRail from "../rails/LeftServicesRail";
 import RightContactRail from "../rails/RightContactRail";
 
 export default function HeroVideo() {
-  const VIDEO_SRC = "/videos/QTS.mp4";
+  const VIDEO_SRC = "https://res.cloudinary.com/dwwibqeao/video/upload/4_ei6xcd.mp4";
   const POSTER_SRC = "/images/2ndImage.jpg";
 
   return (
