5992beff75c6a0fe58d090502f56d59afec65ed8
Alizayan <alizayan267@gmail.com>
2025-10-02 15:24:37 +0500

    [Created] Backend Api's , [Updated] Left rail, Our Projects Tab Createt

--- src/app/Contact/page.js
+++ src/app/Contact/page.js
@@ -220,7 +220,7 @@ export default function ContactPage() {
             </ul>
 
             {/* Map */}
-            <div className="w-full h-98 rounded-lg overflow-hidden border">
+            <div className="w-full h-91 rounded-lg overflow-hidden border">
               <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d115837.33475961476!2d55.17128!3d25.093077!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f4343e69c8b11%3A0x92f0bbfc7d82e9a4!2sBusiness%20Bay%20-%20Dubai!5e0!3m2!1sen!2sae!4v1700000000000"
                 width="100%"
